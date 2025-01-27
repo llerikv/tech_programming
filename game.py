7042ca4fb510f0607a2bb5a9b0583c8cfd3266e3
Kim Valeriya <llerikv@icloud.com>
2025-01-28 01:48:19 +0900

    hhhhh

--- game.py
+++ game.py
@@ -2,5 +2,5 @@ import random
 
 x = random.randint(1, 100)
 while (guess := int(input("Угадай число:"))) != x:
- print(" (Горячо!" if abs (guess - x)<10 else "Холодно!")
+ print(" (Горячо!" if abs (guess - x) <10 else "Холодно!")
 print("Вы угадали!")
