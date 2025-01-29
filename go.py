b3cb7647bc8f1171049c71e3a0852c35a71d106c
Kim Valeriya <llerikv@icloud.com>
2025-01-29 23:42:36 +0900

    добавила дискриминант

--- go.py
+++ go.py
@@ -1,3 +1,4 @@
+import math
 def calculator():
     print("Выберите операцию:")
     print("1. +")
@@ -8,11 +9,12 @@ def calculator():
     print("6. /")
     print("7. pow")
     print("8. cube")
+    print("9. disc")
     
-    choice = input("Введите номер операции (1/2/3/4/5/6/7/8): ")
-    
-    x = float(input("Введите первое число: "))
-    y = float(input("Введите второе число: "))
+    choice = input("Введите номер операции (1/2/3/4/5/6/7/8/9): ")
+    if choice in ['1', '2', '3', '4', '6']:
+       x = float(input("Введите первое число: "))
+       y = float(input("Введите второе число: "))
     
     if choice == '1':
         print(f"{x} + {y} = {x + y}")
@@ -23,6 +25,7 @@ def calculator():
     elif choice == '4':
         print(f"{x} % {y} = {x % y}")
     elif choice == '5':
+        x = float(input("Введите число для вычисления квадратного корня:"))
         sqrt = x ** (0.5) 
         print(f"{x} = {sqrt}")
     elif choice == '6':
@@ -31,9 +34,26 @@ def calculator():
         else:
             print("Ошибка!Деление на ноль нельзя!")
     elif choice == '7':
+        x = float(input("Введите число для возведения в квадрат:"))
         print(f"{x} ** {2} = {x ** 2}")
     elif choice == '8':
+        x = float(input("Введите число для возведение в куб:"))
         print(f"{x} ** {3} = {x ** 3}")      
+    if choice == '9':
+        a = float(input("Введите коэффициент a: "))
+        b = float(input("Введите коэффициент b: "))
+        c = float(input("Введите коэффициент c: "))       
+        D = b**2 - 4*a*c
+        print(f"Дискриминант = {D}")       
+    if D > 0:
+        x1 = (-b + math.sqrt(D) / (2*a))
+        x2 = (-b - math.sqrt(D) / (2*a))
+        print(f"Корни уровнения:x1 и x2 = {x1} , {x2}")
+    elif D == 0:
+        x = -b / (2*a)
+        print(f"Единственный корень: x = {x}")
+    elif D < 0:
+        print("Корней нет (D < 0)")
     else:
         print("Неверный ввод!")
         
