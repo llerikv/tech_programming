4423d72f50b006e4355fc277fe2bbd570bd46ffd
Kim Valeriya <llerikv@icloud.com>
2025-02-05 22:10:33 +0900

    изменила

--- score2.py
+++ score2.py
@@ -1,6 +1,6 @@
 score=int(input("Введите оценку за тест:"))
 
-if score > 90:
+if score >= 90:
     homework=input("Сдали ли студент все домашние задания?(да/нет):")
     if homework=="да":
       print ("Отлично!Оценка A+")
@@ -8,12 +8,12 @@ if score > 90:
         print ("Хорошая работа,но сдайте все задания!Оценка:А")
 
 
-if score > 80:
+elif 80 <= score <= 89 :
     attendance=input("Посещал ли ты более 75% занятий?(да/нет):")
     if attendance=="да":
-      print ("Хорошо!Оценка B")
+        print ("Хорошо!Оценка B")
     elif attendance=="нет":
         print ("Нужно посещать занятия!Оценка С")
     
-    else: 
-        print ("Старайтесь лучше!")
+else: 
+    print ("Старайтесь лучше!")
